b3732a145214da2ebe8a30688f115f5bf2d340cf
Jenane Rajkumar <jenane8521@gmail.com>
2024-07-27 18:37:01 +0530

    Updated the index.html with new features

--- script/script.js
+++ script/script.js
@@ -1,19 +1,14 @@
-// add class navbarDark on navbar scroll
-const header = document.querySelector('.navbar');
-console.log(header)
 window.onscroll = function() {
     const top = window.scrollY;
-    if(top >=100) {
-        header.classList.add('navbarDark');
+    if(top >= 100) {
+        document.querySelector('.navbar').classList.add('navbarDark');
+    } else {
+        document.querySelector('.navbar').classList.remove('navbarDark');
     }
-    else {
-        header.classList.remove('navbarDark');
-    }
-}
-// collapse navbar after click on small devices
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarSupportedContent')
+};
 
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
-})
+document.querySelectorAll('.nav-item').forEach((l) => {
+    l.addEventListener('click', () => {
+        new bootstrap.Collapse(document.getElementById('navbarSupportedContent')).toggle();
+    });
+});
